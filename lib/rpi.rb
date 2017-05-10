d56d9c384e70d01504a5ebfbf3a6a0d88edf4d78
Your Name <you@example.com>
2017-05-10 19:50:09 +0100

    improvement: Replaced the wiringpi gem with the rpi_pinout gem for the sake of code simplicity.

--- lib/rpi.rb
+++ lib/rpi.rb
@@ -2,103 +2,11 @@
 
 # file: rpi.rb
 
-require 'pi_piper'
+require 'rpi_pinout'
 
 
 class RPi
-  include PiPiper
-  
-  @leds = []
-
-  class PinX < Pin
-
-    HIGH = 1
-    LOW = 0
-
-    def initialize(id)
-      @id = id
-      super(pin: id, direction: :out)
-      @on, @blinking = false, false
-    end
-
-    def on(durationx=nil, duration: nil)
-      super(); 
-      @on = true      
-      duration ||=  durationx
-      
-      @off_thread.exit if @off_thread
-      @on_thread = Thread.new {(sleep duration; self.off()) } if duration
-
-    end
-
-    def off(durationx=nil, duration: nil)
-
-      super();
-      return if @internal_call
-      
-      @on, @blinking = false, false      
-      duration ||=  durationx
-      
-      @on_thread.exit if @on_thread
-      @off_thread = Thread.new { (sleep duration; self.on()) } if duration
-
-    end
     
-    alias high on # opposite of low
-    alias open on # opposite of close
-    alias lock on # opposite of unlock
-    
-    alias stop off        
-    alias low off
-    alias close off
-    alias unlock off
-
-    def blink(seconds=0.5, duration: nil)
-
-      @blinking = true
-      t2 = Time.now + duration if duration
-
-      Thread.new do
-        while @blinking do
-
-          set_pin HIGH
-          sleep seconds / 2.0
-          break if !@blinking
-          sleep seconds / 2.0 
-          break if !@blinking
-          
-          set_pin LOW;
-          sleep seconds / 2.0
-          break if !@blinking
-          sleep seconds / 2.0
-
-          break if !@blinking
-          
-          self.off if duration and Time.now >= t2
-        end
-        
-      end
-    end
-    
-    alias oscillate blink
-
-    def on?()  @on  end
-    def off?() !@on end
-
-    # set val with 0 (off) or 1 (on)
-    #
-    def set_pin(val)
-
-      @internal_call = true
-      self.update_value val
-      @internal_call = false
-    end
-    
-    def to_s()
-      @id
-    end
-  end
-  
   class Void
     def on(duration=nil)               end
     def off()                          end
@@ -118,58 +26,24 @@ class RPi
     when Array
       x
     end
-
-    unexport_all a
     
-    @pins = a.map {|pin| PinX.new pin }
+    @pins = a.map {|pin| RPiPinOut.new pin }
     
     def @pins.[](i)
 
-      if i.respond_to? :to_i and i.to_i >= self.length then
-        puts "RPi warning: PinX instance #{i.inspect} not found"
+      if i.to_i >= self.length then
+        puts "RPi warning: RPiPinOut instance #{i.inspect} not found"
         Void.new
       else
-        super(i)
+        self.at(i)
       end 
     end    
-    
-    at_exit do
-      
-      # to avoid "Device or resource busy @ fptr_finalize - /sys/class/gpio/export"
-      # we unexport the pins we used
-      
-      unexport_all a
-    end    
+  
   end
 
   def pin()   @pins.first  end
   def pins()  @pins        end
     
-  def unexport_all(pins)
-    
-    pins.each do |pin|
-      
-      next unless File.exists? '/sys/class/gpio/gpio' + pin.to_s
-
-      uexp = open("/sys/class/gpio/unexport", "w")
-      uexp.write(pin)
-      uexp.close
-    end
-    
-  end
-  
-  def on_exit
-    unexport_all @pins
-  end
-  
-  def self.unexport(a)
-    a.each do |pin|
-      
-      uexp = open("/sys/class/gpio/unexport", "w")
-      uexp.write(pin)
-      uexp.close
-    end    
-  end
 
 end
 
--- rpi.gemspec
+++ rpi.gemspec
@@ -1,14 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'rpi'
-  s.version = '0.4.8'
+  s.version = '0.5.0'
   s.summary = 'The RPi gem makes it easy to set the GPIO pins on the Raspberry Pi e.g. RPi.new([4,27]).pins.first.on'
   s.authors = ['James Robertson']
-  s.add_runtime_dependency('pi_piper', '~> 1.3', '>=1.3.2')
+  s.add_runtime_dependency('rpi_pinout', '~> 0.1', '>=0.1.1')
   s.files = Dir['lib/rpi.rb']
   s.signing_key = '../privatekeys/rpi.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@r0bertson.co.uk'
+  s.email = 'james@jamesrobertson.eu'
   s.homepage = 'https://github.com/jrobertson/rpi'
   s.required_ruby_version = '>= 2.1.2'
 end
